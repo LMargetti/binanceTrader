af6e5e4d7d3bdb9e1cb4f73faefdc36edb87df36
LixumFlorg <69194292+Pa-ulDirac@users.noreply.github.com>
2022-03-27 09:00:22 +0100

    Learning
    currently learning the libraries and options, so it's a mess right now.

--- test.py
+++ test.py
@@ -1,19 +1,35 @@
-import timeit
-
-my_setup = '''
+# imports
 from binance.client import Client
 from decouple import config
-'''
+from time import sleep
+from binance import ThreadedWebsocketManager
 
-my_code = '''
+
+def btc_trade_history(msg):
+    if msg['e'] != 'error':
+        print(msg['c'])
+        btc_price['last'] = msg['c']
+        btc_price['bid'] = msg['b']
+        btc_price['last'] = msg['a']
+        btc_price['error'] = False
+    else:
+        btc_price['error'] = True
+
+
 test_key = config('test_key')
 test_secret = config('test_secret')
 
+# initialise the client
 client = Client(test_key, test_secret)
 client.API_URL = 'https://testnet.binance.vision/api'
-# print(client.get_asset_balance(asset="BTC"))
-'''
+print(client.get_asset_balance(asset="BTC"))
 
-print(timeit.timeit(stmt=my_code,
-                    setup=my_setup,
-                    number=100))
+btc_price = client.get_symbol_ticker(symbol="BTCUSDT")
+print(btc_price)
+
+
+bsm = ThreadedWebsocketManager()
+
+bsm.start_symbol_ticker_socket(callback=btc_trade_history(),
+                               symbol='BTCUSDT')
+
